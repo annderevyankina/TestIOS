e3159624a9f2e1f1faf21d67a94ff69c0ca3dd1c
annderevyankina <anka73der@gmail.com>
2024-11-13 10:44:05 +0300

    Initial Commit

---
+++ Workshop/WorkshopApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkshopApp.swift
+//  Workshop
+//
+//  Created by Анна on 13.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct WorkshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
